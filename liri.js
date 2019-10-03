3ce6164b782e31f321fd535bde99a8302938a576
dayne beronque <dayneyulber@gmail.com>
2019-10-03 18:26:44 -0500

    calls to applications

--- liri.js
+++ liri.js
@@ -7,3 +7,13 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var operand = process.argv[2];
 var input = process.argv.slice(3).join(" ");
+
+if (operand === "movie-this") {
+    movieThis(input);
+} else if (operand === "concert-this") {
+    concertThis(input);
+} else if (operand === "spotify-this-song") {
+    spotifyThis(input);
+} else if (operand === "do-what-it-says") {
+    doThis(input)
+}
