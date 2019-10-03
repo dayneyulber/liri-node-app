f7a3d5962915be1413592980df1c4ccfa02214b6
dayne beronque <dayneyulber@gmail.com>
2019-10-03 17:48:03 -0500

    installed packages

--- liri.js
+++ liri.js
@@ -1,2 +1,9 @@
-var inquire = require("axios");
-
+require("dotenv").config();
+var keys = require("./keys.js");
+var fs = require("fs");
+var axios = require("axios");
+var moment = require("moment");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var operand = process.argv[2];
+var input = process.argv.slice(3).join(" ");
