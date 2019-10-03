819c66ca5937818d1c014ad212596e1d1e772e67
dayne beronque <dayneyulber@gmail.com>
2019-10-03 18:46:19 -0500

    movie-this function

--- liri.js
+++ liri.js
@@ -17,3 +17,37 @@ if (operand === "movie-this") {
 } else if (operand === "do-what-it-says") {
     doThis(input)
 }
+
+function movieThis(movie) {
+    if (!movie) {
+        movie = "Mr. Nobody";
+        console.log("If you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/>");
+        console.log("It's on Netflix!")
+    }
+    axios
+        .get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
+        .then(function (response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating[0].Value);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            }
+            else if (error.request) {
+                console.log(error.request);
+            }
+            else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
