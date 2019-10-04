9e068cf8ab0f41f4a94c530b63cfd7b4e2c0fc71
dayne beronque <dayneyulber@gmail.com>
2019-10-03 20:06:11 -0500

    Spotify ID + secret added

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "3ac6a83b15094e4493c90ee41ed1d7ac",
+  secret: "37d77ee999c74dfbb7227fce53dbbe00"
 };
